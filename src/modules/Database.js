6f7bb3a6388d714597cce0d26246bbeb6f489ba1
David Pratte <d.pratte@napec.ca>
2018-11-07 14:56:12 -0500

    add support for match notes!

--- src/modules/Database.js
+++ src/modules/Database.js
@@ -5,7 +5,90 @@ const dbPromise = Promise.resolve()
     .then(db => db.migrate({}));
 
 
-exports.scheduledIntervalList = async (pk) => {
+exports.matchNotesList = async (maxRank = 0) => {
+    const db = await dbPromise;
+
+    if(maxRank > 0){
+        return await db.all('SELECT * FROM MatchNotes WHERE permLevel <= ?', maxRank);
+    }
+
+    return await db.all('SELECT * FROM MatchNotes WHERE permLevel <= 6');
+};
+exports.matchNotesPutEpisode = async(pk, data) => {
+    const db = await dbPromise;
+
+    const result = await db.run(`INSERT INTO MatchNotes(
+        episodeId,
+        permLevel,
+        memberId, 
+        created,
+        updated,
+        note) VALUES(?,?,?,?,?,?)`,
+        data.episodeId,
+        data.permLevel,
+        data.memberId, 
+        data.created,
+        data.updated,
+        data.note);
+
+    console.log(result);
+    return result;
+
+};
+exports.matchNotesNew = () => {
+    return {
+        created: new Date().toISOString(),
+        updated: new Date().toISOString(),
+        id: 0,
+        permLevel: 10,
+        memberId: 0,
+        episodeId: 0,
+        note: ""
+    }
+}
+
+
+exports.matchNotesPut = async (pk, data) => {
+    const db = await dbPromise;
+
+    await db.run(`REPLACE INTO MatchNotes(
+        id,
+        episodeId,
+        permLevel,
+        memberId, 
+        created,
+        updated,
+        note) VALUES(?,?,?,?,?,?,?)`, 
+        pk, 
+        data.episodeId,
+        data.permLevel,
+        data.memberId, 
+        data.created,
+        data.updated,
+        data.note);
+
+};
+
+exports.matchNotesGetEpisode = async (pk) => {
+    const db = await dbPromise;
+    return await db.all('SELECT * FROM MatchNotes WHERE episodeId = ?', pk);
+};
+
+exports.matchNotesGet = async (pk) => {
+    const db = await dbPromise;
+    return await db.get('SELECT * FROM MatchNotes WHERE id = ?', pk);
+}
+
+exports.matchNotesRemove = async (pk) => {
+    const db = await dbPromise;
+    await db.run('DELETE FROM MatchNotes WHERE id = ?', pk);
+}
+exports.matchNotesRemoveEpisode = async (pk) => {
+    const db = await dbPromise;
+    await db.run('DELETE FROM MatchNotes WHERE episodeId = ?', pk);
+}
+
+exports.scheduledIntervalList = async () => {
     const db = await dbPromise;
     const channelInfo = await db.all('SELECT * FROM ScheduleInterval');
 
