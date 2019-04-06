8420b43eb82549ce01b545413ea150709d99215f
hancin <hancin@hancinworld.com>
2019-04-06 09:48:22 -0400

    Fix how the restreamers are hidden

--- src/commands/schedule.js
+++ src/commands/schedule.js
@@ -189,7 +189,7 @@ ${showRestreamers(x,2,expectedCrew)} _${x.variations}_`
             });
         } else {
             var needsObject = [{name: "Commentary", text: "commentator", value: {}}, 
-            {name: "Tracking", text: "tracker", value: {}}/*, {name: "Restreaming", text:"restreamer", value: {}}*/];
+            {name: "Tracking", text: "tracker", value: {}}, {name: "Restreaming", text:"restreamer", value: {}}];
 
             selected.forEach((ep) => {
                 let expectedCrew = [2, ep.playerInfo.value.length > 2? 2:1, ep.channelName.indexOf("SpeedGaming")!== -1 ? 0: 1];
@@ -205,6 +205,9 @@ ${showRestreamers(x,2,expectedCrew)} _${x.variations}_`
             });
 
             needsObject.forEach((section) => {
+                if(section.text === "restreamer")
+                    return;
+                    
                 let times = Object.keys(section.value);
                 let values = times.map((t) => `${t} - ${section.value[t]} ${section.text}${section.value[t] !== 1?'s':''}`);
 
